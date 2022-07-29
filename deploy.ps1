ea74dd10854314ae7bc8d2dc674895447988036f
Aleksandr Riabov <zkelo@yandex.ru>
2022-07-29 22:25:43 +0300

    Обновлены скрипты

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,18 @@
 . .\scripts\vars.ps1
 
-# Сборка и компиляция скрипта
+<# Перменные #>
+# Путь к временной папке
+$DistFolder = 'tmp'
+
+# Название файла со скриптом из $ScriptPath
+$ScriptFilename = Split-Path $ScriptPath -leaf
+
+# Полный путь к скомилированному скрипту во временной папке
+$CompiledScriptPath = "..\$DistFolder\$($ScriptFilename)c"
+
+<# Код #>
+New-Item -ItemType 'directory' -Name $DistFolder -Path './'
+
+cd .\luajit
+& '.\luajit.exe' '-b' ".$ScriptPath" $CompiledScriptPath
+cd ..

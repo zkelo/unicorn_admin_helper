a8526f4e9bd8c37e7570e2e1eaba189f3bf8f55c
Aleksandr Riabov <zkelo@yandex.ru>
2022-07-29 22:29:42 +0300

    Обновлён скрипт

--- deploy.ps1
+++ deploy.ps1
@@ -2,13 +2,18 @@
 
 <# Перменные #>
 # Путь к временной папке
-$DistFolder = 'tmp'
+$DistFolder = 'dist'
 
 # Название файла со скриптом из $ScriptPath
 $ScriptFilename = Split-Path $ScriptPath -leaf
 
 # Полный путь к скомилированному скрипту во временной папке
 $CompiledScriptPath = "..\$DistFolder\$($ScriptFilename)c"
+
+if (Test-Path -Path $CompiledScriptPath -PathType leaf)
+{
+    Remove-Item -Force $CompiledScriptPath
+}
 
 <# Код #>
 New-Item -ItemType 'directory' -Name $DistFolder -Path './'

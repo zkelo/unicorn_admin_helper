7d68937d773e53fcb8b6a240a90533674db30a44
Aleksandr Riabov <zkelo@yandex.ru>
2022-07-30 11:30:21 +0300

    Обновлён скрипт компиляции скрипта :)

--- build.ps1
+++ build.ps1
@@ -10,6 +10,8 @@ $CompiledScriptPath = ".\$DistFolder\$CompiledScriptFilename"
 $CompiledScriptGameFolderPath = "$GamePath\moonloader\$CompiledScriptFilename"
 
 <# Код #>
+Write-Output 'Выполняется компиляция скрипта...'
+
 if (Test-Path -Path $CompiledScriptPath -PathType leaf)
 {
     Remove-Item -Force $CompiledScriptPath
@@ -19,14 +21,31 @@ cd .\luajit
 & '.\luajit.exe' '-b' ".$ScriptPath" ".$CompiledScriptPath"
 cd ..
 
+if (-not (Test-Path -Path $CompiledScriptPath -PathType leaf))
+{
+    Write-Error -Message 'Не удалось скомпилировать скрипт' -RecommendedAction 'Проверьте код на ошибки и запустите комиляцию снова' -Category SyntaxError
+    Exit 1
+}
+else
+{
+    Write-Output 'Скрипт успешно скомпилирован'
+}
+
+Write-Output 'Копирование скрипта в папку moonloader игры...'
+
 Try
 {
     '' | Out-File $CompiledScriptGameFolderPath
 }
 Catch
 {
-    Write-Error 'Нет прав на запись в папку игры'
+    Write-Error 'Нет прав на запись в папку игры' -RecommendedAction 'Попробуйте запустить скрипт компиляции с правами администратора' -Category PermissionDenied
 }
 
 Remove-Item -Force $CompiledScriptGameFolderPath
 Copy-Item -Force -Path $CompiledScriptPath -Destination $CompiledScriptGameFolderPath
+
+if (Test-Path -Path $CompiledScriptGameFolderPath -PathType leaf)
+{
+    Write-Host 'Скрипт успешно скопирован' -ForegroundColor Green
+}
